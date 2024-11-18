b1ca0b1e5430d593d7899b43f3aeaa6d573d1b01
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2024-11-18 15:28:44 +0200

    part02-06 solution

--- part02-06_elder/src/elder.py
+++ part02-06_elder/src/elder.py
@@ -1,1 +1,13 @@
-# Write your solution here
+print("Person 1:")
+name_p1 = input("Name:") 
+age_p1 = int(input("Age:"))
+print("Person 2:")
+name_p2 = input("Name:")
+age_p2 = int(input("Age:"))
+
+if age_p1 == age_p2:
+    print(f"{name_p1} and {name_p2} are the same age")
+elif age_p1 > age_p2:
+    print(f"The elder is {name_p1}")
+else:
+    print(f"The elder is {name_p2}")
