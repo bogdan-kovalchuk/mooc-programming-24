3bdf17d77678d338c72440ee9f4f16583e42a1db
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2024-11-18 15:12:40 +0200

    part02-05 solution

--- part02-05_greater_or_equal/src/greater_or_equal.py
+++ part02-05_greater_or_equal/src/greater_or_equal.py
@@ -1,1 +1,7 @@
-# Write your solution here
+num1 = int(input("Please type in the first number"))
+num2 = int(input("Please type in another number:"))
+
+if num1 == num2:
+    print(f"The numbers are equal!")
+else:
+    print(f"The greater number was: {max(num1, num2)}")
