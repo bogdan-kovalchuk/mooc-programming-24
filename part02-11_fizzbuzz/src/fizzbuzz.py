c7b7033868916ca1e2c24f0cfdcbb495f5cbec99
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2024-11-20 10:58:29 +0200

    part02-11 solution

--- part02-11_fizzbuzz/src/fizzbuzz.py
+++ part02-11_fizzbuzz/src/fizzbuzz.py
@@ -1,1 +1,8 @@
-# Write your solution here
+number = int(input("Number:"))
+
+if number % 5 == 0 and number % 3 == 0:
+    print("FizzBuzz")
+elif number % 5 == 0:
+    print("Buzz")
+elif number % 3 == 0:
+    print("Fizz")
