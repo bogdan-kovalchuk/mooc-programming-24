20532631680a6dd41048a313ae164a0931f350a2
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2024-11-21 14:26:38 +0200

    part02-13 solution

--- part02-13_alphabetically_in_the_middle/src/alphabetically_in_the_middle.py
+++ part02-13_alphabetically_in_the_middle/src/alphabetically_in_the_middle.py
@@ -1,1 +1,13 @@
-# Write your solution here
+letter1 = input("1st letter:")
+letter2 = input("2nd letter:")
+letter3 = input("3rd letter:")
+
+letter = None
+if letter1 > letter2 > letter3 or letter1 < letter2 < letter3:
+    letter = letter2
+elif letter1 > letter3 > letter2 or letter1 < letter3 < letter2:
+    letter = letter3
+elif letter2 > letter1 > letter3 or letter2 < letter1 < letter3:
+    letter = letter1
+
+print(f"The letter in the middle is {letter}")
