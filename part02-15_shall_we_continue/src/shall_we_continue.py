33f86b128c3bbcca473b620e9f5bae9078f753a0
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2024-11-22 12:09:04 +0200

    part02-15 solution

--- part02-15_shall_we_continue/src/shall_we_continue.py
+++ part02-15_shall_we_continue/src/shall_we_continue.py
@@ -1,1 +1,8 @@
-# Write your solution here
+while True:
+    print("hi")
+    answer = input("Shall we continue?")
+    if answer == 'yes' or answer == 'oui' or answer == 'jawohl':
+        continue
+    elif answer == 'no':
+        print("okay then")
+        break
