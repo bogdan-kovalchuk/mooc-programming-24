6f2fd54726c0b59403b9fad55bac65363c7845d6
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2024-11-22 15:29:40 +0200

    part02-20 solution

--- part02-20_next_leap_year/src/next_leap_year.py
+++ part02-20_next_leap_year/src/next_leap_year.py
@@ -1,1 +1,10 @@
-# Write your solution here
+year = int(input("Year:"))
+leap_year = year + 1
+
+while True:
+    if (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0):
+        print(f"The next leap year after {year} is {leap_year}")
+        break
+    leap_year += 1
+
+    
