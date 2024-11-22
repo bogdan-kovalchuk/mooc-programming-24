b17545f60dec2fe027f519abfae847d18a77fdb8
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2024-11-22 15:08:10 +0200

    part02-19 solution

--- part02-19_pin_and_number_of_attempts/src/pin_and_number_of_attempts.py
+++ part02-19_pin_and_number_of_attempts/src/pin_and_number_of_attempts.py
@@ -1,1 +1,17 @@
-# Write your solution here
+pin = "4321"
+
+attemps = 0
+while True:
+    entered_pin = input("PIN:")
+    attemps += 1
+    if pin == entered_pin:
+        if attemps == 1:
+            print("Correct! It only took you one single attempt!")
+        else:
+            print(f"Correct! It took you {attemps} attempts")
+        break
+    else:
+        print("Wrong")
+
+
+    
