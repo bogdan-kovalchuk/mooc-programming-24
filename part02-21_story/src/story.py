a06430e929daecfbe74ac7cba1e65ee0cb84ba38
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2024-11-22 15:39:29 +0200

    part02-21 solution

--- part02-21_story/src/story.py
+++ part02-21_story/src/story.py
@@ -1,1 +1,9 @@
-# Write your solution here
+story = ""
+previous_word = ""
+while True:
+    word = input("Please type in a word:")
+    if word == 'end' or previous_word == word:
+        print(story)
+        break
+    story += word + " "
+    previous_word = word
