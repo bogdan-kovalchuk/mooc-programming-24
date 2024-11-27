347fadfaace1ba2e8dec611dca149aa4d934c89c
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2024-11-27 17:03:01 +0200

    part02-22 solution

--- part02-22_working_with_numbers/src/working_with_numbers.py
+++ part02-22_working_with_numbers/src/working_with_numbers.py
@@ -1,1 +1,22 @@
-# Write your solution here
+n_numbers = 0
+numbers_sum = 0
+positive_numbers = 0
+negative_numbers = 0
+print("Please type in integer numbers. Type in 0 to finish.")
+while True:
+    number = int(input("Number:"))
+    if number == 0:
+        break
+    n_numbers += 1
+    numbers_sum += number
+    if number > 0:
+        positive_numbers += 1
+    else:
+        negative_numbers += 1
+
+
+print(f"Numbers typed in {n_numbers}")
+print(f"The sum of the numbers is {numbers_sum}")
+print(f"The mean of the numbers is {numbers_sum/n_numbers}")
+print(f"Positive numbers {positive_numbers}")
+print(f"Negative numbers {negative_numbers}")
