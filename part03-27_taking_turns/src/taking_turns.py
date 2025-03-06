9b26cb2e956ae749117e528539933c4351196054
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-06 18:43:40 +0200

    part02-27 solution

--- part03-27_taking_turns/src/taking_turns.py
+++ part03-27_taking_turns/src/taking_turns.py
@@ -1,1 +1,12 @@
-# Write your solution here
+# Taking turns
+number = int(input("Please type in a number:"))
+i = 1
+j = number
+while i <= j:
+    if i == j:
+        print(i)
+    else:
+        print(i)
+        print(j)
+    i += 1
+    j -= 1
