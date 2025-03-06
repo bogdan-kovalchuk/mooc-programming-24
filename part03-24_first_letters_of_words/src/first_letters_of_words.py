7665e3cedcffac7de06418187a073164f6addad7
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-06 10:01:44 +0200

    part03-24 solution

--- part03-24_first_letters_of_words/src/first_letters_of_words.py
+++ part03-24_first_letters_of_words/src/first_letters_of_words.py
@@ -1,1 +1,9 @@
-# Write your solution here
+# First letters of words
+sentence = input("Please type in a sentence:")
+
+if len(sentence) > 0:
+    print(sentence[0])
+    idx = sentence.find(" ")
+    while idx != -1:
+        print(sentence[idx + 1])
+        idx = sentence.find(" ", idx + 1)
