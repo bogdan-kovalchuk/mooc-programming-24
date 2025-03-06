44777335c6e73334a67ad4ca3a25aff875c6a298
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-06 12:17:12 +0200

    part03-25 solution

--- part03-25_factorial/src/factorial.py
+++ part03-25_factorial/src/factorial.py
@@ -1,1 +1,13 @@
-# Write your solution here
+# Factorial
+while True:
+    number = int(input("Please type in a number:"))
+    if number <= 0:
+        print("Thanks and bye!")
+        break
+    else:
+        n = number
+        factorial = 1
+        while number >= 1:
+            factorial *= number
+            number -= 1
+        print(f"The factorial of the number {n} is {factorial}")
