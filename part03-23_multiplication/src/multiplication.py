8c93883649d38192a1921ad5d965ca5249bfb4b6
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-06 08:51:34 +0200

    part03-23 solution

--- part03-23_multiplication/src/multiplication.py
+++ part03-23_multiplication/src/multiplication.py
@@ -1,1 +1,5 @@
-# Write your solution here
+# Multiplication
+number = int(input("Please type in a number:"))
+for i in range(1, number + 1):
+    for j in range(1, number + 1):
+        print(f"{i} x {j} = {i*j}")
