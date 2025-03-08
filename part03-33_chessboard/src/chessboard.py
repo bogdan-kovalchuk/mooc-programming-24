60d22d6d2c1d69eab4af8bfa3e14468fbf3c0228
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-08 17:53:47 +0200

    part03-33 solution

--- part03-33_chessboard/src/chessboard.py
+++ part03-33_chessboard/src/chessboard.py
@@ -1,5 +1,10 @@
-# Write your solution here
+# Chessboard
+def chessboard(num: int):
+    for i in range(num):
+        row = "".join("1" if (i + j) % 2 == 0 else "0" for j in range(num))
+        print(row)
 
-# Testing the function
+
 if __name__ == "__main__":
     chessboard(3)
+    chessboard(6)
