4447dc7bc3bfe8efeb24cc772fb3cfeb1bfee04d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-08 16:20:11 +0200

    part03-29 solution

--- part03-29_first_character/src/first_character.py
+++ part03-29_first_character/src/first_character.py
@@ -1,11 +1,12 @@
 def first_character(text):
-# write your code here
-#
-# testing the function:
+    if len(text) > 0:
+        print(text[0])
+
+
 if __name__ == "__main__":
-first_character('python')
-first_character('yellow')
-first_character('tomorrow')
-first_character('heliotrope')
-first_character('open')
-first_character('night')
+    first_character("python")
+    first_character("yellow")
+    first_character("tomorrow")
+    first_character("heliotrope")
+    first_character("open")
+    first_character("night")
