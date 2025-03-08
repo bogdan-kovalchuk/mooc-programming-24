8e4d5c64a48fa27fde88ca11d66545c0b6213379
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-08 16:24:39 +0200

    part03-31 solution

--- part03-31_print_many_times/src/print_many_times.py
+++ part03-31_print_many_times/src/print_many_times.py
@@ -1,5 +1,8 @@
-# Write your solution here
+# Print many times
+def print_many_times(string, times):
+    for i in range(times):
+        print(string)
 
-# You can test your function by calling it within the following block
+
 if __name__ == "__main__":
     print_many_times("python", 5)
