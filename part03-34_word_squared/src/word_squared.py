7c8c08606e89d5b65fdc37cb864f9b2776a0e3e3
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-08 18:16:56 +0200

    part03-34 solution

--- part03-34_word_squared/src/word_squared.py
+++ part03-34_word_squared/src/word_squared.py
@@ -1,1 +1,13 @@
-# Write your solution here
+# A word squared
+def squared(string: str, num: int) -> None:
+    for i in range(num):
+        row = ""
+        for j in range(num):
+            row += string[(i * num + j) % len(string)]
+        print(row)
+
+
+if __name__ == "__main__":
+    squared("ab", 3)
+    print()
+    squared("aybabtu", 5)
