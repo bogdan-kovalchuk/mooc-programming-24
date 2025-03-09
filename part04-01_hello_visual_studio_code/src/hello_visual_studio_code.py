a7f29c7ea39038515065956faaecb17514cb9024
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-09 08:00:21 +0200

    part04-01 solution

--- part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,10 @@
-# Write your solution here
+# Hello Visual Studio Code
+while True:
+    editor = input("Editor:").lower()
+    if editor in ["word", "notepad"]:
+        print("awful")
+    elif editor == "visual studio code":
+        print("an excellent choice!")
+        break
+    else:
+        print("not good")
