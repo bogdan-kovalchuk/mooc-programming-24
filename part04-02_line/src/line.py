8381fb9956c0f976690003f06b146c2fbe4c49cc
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-09 09:26:12 +0200

    part04-02 solution

--- part04-02_line/src/line.py
+++ part04-02_line/src/line.py
@@ -1,4 +1,10 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+# Line
+def line(num: int, string: str) -> None:
+    char = "*"
+    if string:
+        char = string[0]
+    print(num * char)
+
+
 if __name__ == "__main__":
     line(5, "x")
