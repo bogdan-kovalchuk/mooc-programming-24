33e8ba92846ff0fa6ccff7de302b4ce0e5c21894
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-09 12:16:54 +0200

    part04-04

--- part04-04_square_of_hashes/src/square_of_hashes.py
+++ part04-04_square_of_hashes/src/square_of_hashes.py
@@ -1,9 +1,15 @@
-# Copy here code of line function from previous exercise
+# A square of hashes
+def line(num: int, string: str) -> None:
+    char = "*"
+    if string:
+        char = string[0]
+    print(num * char)
+
 
 def square_of_hashes(size):
-    # You should call function line here with proper parameters
-    line(4, "#")
+    for i in range(size):
+        line(size, "#")
 
-# You can test your function by calling it within the following block
+
 if __name__ == "__main__":
     square_of_hashes(5)
