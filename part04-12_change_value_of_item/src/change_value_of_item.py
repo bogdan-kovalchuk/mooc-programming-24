89511b259da9beccb63896d177788317b4757b32
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-10 15:41:55 +0200

    part04-12 solution

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,7 @@
-# Write your solution here
+lst = [1, 2, 3, 4, 5]
+while True:
+    idx = int(input("Index:"))
+    if idx == -1:
+        break
+    lst[idx] = int(input("New value:"))
+    print(lst)
