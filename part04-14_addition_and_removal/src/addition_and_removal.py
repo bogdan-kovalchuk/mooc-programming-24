272ae759c4eef443110908e6f0d9b7ecd751b848
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-10 17:41:29 +0200

    part04-14 solution

--- part04-14_addition_and_removal/src/addition_and_removal.py
+++ part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,13 @@
-# Write your solution here
+lst = []
+while True:
+    print(f"The list is now {lst}")
+    user_input = input("a(d)d, (r)emove or e(x)it: ").lower()
+
+    if user_input == "d":
+        lst.append(lst[-1] + 1 if lst else 1)
+    elif user_input == "r" and lst:
+        lst.pop()
+    elif user_input == "x":
+        break
+
+print("Bye!")
