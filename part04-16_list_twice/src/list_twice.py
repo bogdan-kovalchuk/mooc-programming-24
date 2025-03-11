15dcd9217161b0e506d9978a7db2c1abbb58f8c4
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-11 10:58:01 +0200

    part04-16 solution

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,8 @@
-# Write your solution here
+lst = list()
+while True:
+    if (item := int(input("New item: "))) == 0:
+        break
+    lst.append(item)
+    print(f"The list now: {lst}")
+    print(f"The list in order: {sorted(lst)}")
+print("Bye!")
