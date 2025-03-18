7602b071f80fb018647ef6fc18e9d2fe393e4df6
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-18 12:43:43 +0200

    part04-27 solution

--- part04-27_sum_of_lists/src/sum_of_lists.py
+++ part04-27_sum_of_lists/src/sum_of_lists.py
@@ -1,2 +1,8 @@
 def list_sum(a: list, b: list) -> list:
-    return a + b
+    return [i + j for i, j in zip(a, b)]
+
+
+if __name__ == "__main__":
+    a = [1, 2, 3]
+    b = [7, 8, 9]
+    print(list_sum(a, b))  # [8, 10, 12]
