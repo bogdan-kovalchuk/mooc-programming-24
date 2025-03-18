dda3eaca401757fcec67f17dabdfb254d4f4c6ef
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-18 09:14:59 +0200

    part04-23 solution

--- part04-23_anagrams/src/anagrams.py
+++ part04-23_anagrams/src/anagrams.py
@@ -1,1 +1,10 @@
-# Write your solution here
+
+def anagrams(s1: str, s2: str) -> bool:
+    return sorted(s1) == sorted(s2)
+    
+if __name__ == "__main__":
+    print(anagrams("tame", "meta")) # True
+    print(anagrams("tame", "mate")) # True
+    print(anagrams("tame", "team")) # True
+    print(anagrams("tabby", "batty")) # False
+    print(anagrams("python", "java")) # False
