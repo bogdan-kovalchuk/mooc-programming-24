ac262f55a5ce49cd2ce843f74bb4d5a0124ce443
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-18 09:41:59 +0200

    part04-24 solution

--- part04-24_palindromes/src/palindromes.py
+++ part04-24_palindromes/src/palindromes.py
@@ -1,4 +1,11 @@
-# Write your solution here
-# Note, that at this time the main program should not be written inside
-# if __name__ == "__main__":
-# block!
+def palindromes(s: str) -> bool:
+    return s == s[::-1]
+    
+while True:
+    s = input("Please type in a palindrome: ")
+    is_palindrome = palindromes(s)
+    if is_palindrome:
+        print(f"{s} is a palindrome!")
+        break
+    else:
+        print("that wasn't a palindrome")
