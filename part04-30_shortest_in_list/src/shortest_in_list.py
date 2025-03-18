3faf80caac700d099873c66d62b647902c252446
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-18 16:45:20 +0200

    part04-30 solution

--- part04-30_shortest_in_list/src/shortest_in_list.py
+++ part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,12 @@
-# Write your solution here
+def shortest(lst: list) -> str:
+    return sorted(lst, key=lambda x: len(x))[0]
+
+
+if __name__ == "__name__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = shortest(my_list)
+    print(result)
+
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    result = shortest(my_list)
+    print(result)
