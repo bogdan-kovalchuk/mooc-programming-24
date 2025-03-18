6bc610f3c6586d4dd1925ad4ee2e4fda9bb0f3b3
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-18 15:13:36 +0200

    part04-29 solution

--- part04-29_length_of_longest/src/length_of_longest.py
+++ part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,12 @@
-# Write your solution here
+def length_of_longest(lst: list) -> int:
+    return max(list(map(len, lst)))
+
+
+if __name__ == "__main__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = length_of_longest(my_list)
+    print(result)
+
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    result = length_of_longest(my_list)
+    print(result)
