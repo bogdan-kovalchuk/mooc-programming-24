b6b4a0d95b94fed535021844328ad8555c66c75e
bogdan-kovalchuk <bogdan.kovalchuk@gmail.com>
2025-03-19 16:33:23 +0200

    part04-33 solution

--- part04-33_everything_reversed/src/everything_reversed.py
+++ part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,9 @@
-# Write your solution here
+def everything_reversed(string_list):
+    return [s[::-1] for s in string_list][::-1]
+
+
+if __name__ == "__main__":
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
+    
