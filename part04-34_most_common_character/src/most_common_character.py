802c74e72e39512fcc151f7d1d60b5a5bfa30a47
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-20 11:37:18 +0200

    part04-34 solution

--- part04-34_most_common_character/src/most_common_character.py
+++ part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def most_common_character(string: str) -> str:
+    d = {}
+    for ch in string:
+        if ch in d:
+            d[ch] += 1
+        else:
+            d[ch] = 1
+    return max(d, key=d.get)
+
+
+if __name__ == "__main__":
+    first_string = "abcdbde"
+    print(most_common_character(first_string))
+
+    second_string = "exemplaryelementary"
+    print(most_common_character(second_string))
