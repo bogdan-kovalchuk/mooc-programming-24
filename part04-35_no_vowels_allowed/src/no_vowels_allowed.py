a50799c3ed04f4f9ebfc858a31bd0358664f841c
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-20 12:30:40 +0200

    part04-35 solution

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,10 @@
-# Write your solution here
+def no_vowels(string: str) -> str:
+    return "".join([x for x in list(string) if x not in ["a", "e", "i", "o", "u"]])
+
+
+if __name__ == "__main__":
+    my_string = "this is an example"
+    print(no_vowels(my_string))
+
+    my_string = "aeoli"
+    print(no_vowels(my_string))
