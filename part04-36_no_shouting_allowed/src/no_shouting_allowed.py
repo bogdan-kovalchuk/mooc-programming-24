f61d9306248f3d5d7a7ff459868d1341c9cfd915
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-21 08:44:58 +0200

    part04-36 solution

--- part04-36_no_shouting_allowed/src/no_shouting_allowed.py
+++ part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def no_shouting(my_list: list) -> list:
+    return [x for x in my_list if not x.isupper()]
+
+
+if __name__ == "__main__":
+    my_list = [
+        "ABC",
+        "def",
+        "UPPER",
+        "ANOTHERUPPER",
+        "lower",
+        "another lower",
+        "Capitalized",
+    ]
+    pruned_list = no_shouting(my_list)
+    print(pruned_list)
