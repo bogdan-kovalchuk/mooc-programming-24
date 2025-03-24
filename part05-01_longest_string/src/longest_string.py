5796c8ee3388fb7501e8510738d4abf5ba69c76a
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-24 12:31:46 +0200

    part05-01 solution

--- part05-01_longest_string/src/longest_string.py
+++ part05-01_longest_string/src/longest_string.py
@@ -1,1 +1,7 @@
-# Write your solution here
+def longest(strings: list) -> str:
+    return max(strings, key=lambda x: len(x))
+
+
+if __name__ == "__main__":
+    strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+    print(longest(strings))
