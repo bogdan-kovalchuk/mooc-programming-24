f5d6962f5b0007d07f22a639934490a19428518d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-25 08:28:01 +0200

    part05-02 solution

--- part05-02_number_of_elements/src/number_of_elements.py
+++ part05-02_number_of_elements/src/number_of_elements.py
@@ -1,1 +1,12 @@
-# Write your solution here
+def count_matching_elements(m: list, element: int) -> int:
+    out = 0
+    for i in m:
+        for j in i:
+            if j == element:
+                out += 1
+    return out
+
+
+if __name__ == "__main__":
+    m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+    print(count_matching_elements(m, 1))
