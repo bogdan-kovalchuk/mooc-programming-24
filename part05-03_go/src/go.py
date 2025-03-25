c2fe7eb134c992a6598f7176c0cbc9a99642caa1
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-25 09:42:05 +0200

    part05-03 solution

--- part05-03_go/src/go.py
+++ part05-03_go/src/go.py
@@ -1,1 +1,15 @@
-# Write your solution here
+def who_won(game_board: list) -> int:
+    player_one_scores = 0
+    player_two_scores = 0
+    for row in game_board:
+        for val in row:
+            if val == 1:
+                player_one_scores += 1
+            elif val == 2:
+                player_two_scores += 1
+    if player_one_scores > player_two_scores:
+        return 1
+    elif player_one_scores < player_two_scores:
+        return 2
+    else:
+        return 0
