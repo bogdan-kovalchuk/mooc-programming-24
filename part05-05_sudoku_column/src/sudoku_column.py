d9fa92af8f63748e491d8268839a211881a2d94a
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-25 10:01:25 +0200

    part05-05 solution

--- part05-05_sudoku_column/src/sudoku_column.py
+++ part05-05_sudoku_column/src/sudoku_column.py
@@ -1,1 +1,23 @@
-# Write your solution here
+def column_correct(sudoku: list, column_no: int) -> bool:
+    column = [x[column_no] for x in sudoku]
+    for num in range(1, 10):
+        if column.count(num) > 1:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    sudoku = [
+        [9, 0, 0, 0, 8, 0, 3, 0, 0],
+        [2, 0, 0, 2, 5, 0, 7, 0, 0],
+        [0, 2, 0, 3, 0, 0, 0, 0, 4],
+        [2, 9, 4, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 7, 3, 0, 5, 6, 0],
+        [7, 0, 5, 0, 6, 0, 4, 0, 0],
+        [0, 0, 7, 8, 0, 3, 9, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 2],
+    ]
+
+    print(column_correct(sudoku, 0))
+    print(column_correct(sudoku, 1))
