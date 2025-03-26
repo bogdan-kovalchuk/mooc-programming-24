573b40873bded41f9942481d053dc3fe4210a8f5
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-26 12:54:54 +0200

    part05-08 solution and templates

--- part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
+++ part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
@@ -1,1 +1,9 @@
-# Write your solution here
+def double_items(numbers: list) -> list:
+    return list(map(lambda x: 2 * x, numbers))
+
+
+if __name__ == "__main__":
+    numbers = [2, 4, 5, 3, 11, -4]
+    numbers_doubled = double_items(numbers)
+    print("original:", numbers)
+    print("doubled:", numbers_doubled)
