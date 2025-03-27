c62432cac591fc84525b2beeeec846037513ab3e
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-27 11:12:09 +0200

    part05-15 solution

--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,1 +1,12 @@
-# Write your solution here
+def factorials(n: int):
+    fact = {1: 1}
+    for i in range(2, n + 1):
+        fact[i] = i * fact[i - 1]
+    return fact
+
+
+if __name__ == "__main__":
+    k = factorials(5)
+    print(k[1])
+    print(k[3])
+    print(k[5])
