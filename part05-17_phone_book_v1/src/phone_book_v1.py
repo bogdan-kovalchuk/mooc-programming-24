a6ee43854eca3bffae73ef89bbc83d06f580e8fa
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-27 12:25:48 +0200

    part05-17 solution

--- part05-17_phone_book_v1/src/phone_book_v1.py
+++ part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,1 +1,17 @@
-# Write your solution here
+phone_book = {}
+while True:
+    command = int(input("command (1 search, 2 add, 3 quit):"))
+    if command == 1:
+        name = input("name:")
+        if name not in phone_book:
+            print("no number")
+        else:
+            print(phone_book[name])
+    elif command == 2:
+        name = input("name:")
+        number = input("number:")
+        phone_book[name] = number
+        print("ok!")
+    elif command == 3:
+        print("quitting...")
+        break
