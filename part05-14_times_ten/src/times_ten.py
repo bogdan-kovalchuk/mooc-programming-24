61bb25c1a840207b13bf4abe34588858dc1469a6
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-27 10:39:43 +0200

    part05-14 solution

--- part05-14_times_ten/src/times_ten.py
+++ part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,7 @@
-# Write your solution here
+def times_ten(start_index: int, end_index: int):
+    return {i: 10 * i for i in range(start_index, end_index + 1)}
+
+
+if __name__ == "__main__":
+    d = times_ten(3, 6)
+    print(d)
