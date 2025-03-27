5a3de29f16325c4353552808fbdbf0f4494a5baf
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-27 12:07:10 +0200

    part05-16 solution

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,13 @@
-# Write your solution here
+def histogram(string: str):
+    d = {}
+    for letter in string:
+        if letter not in d:
+            d[letter] = 0
+        d[letter] += 1
+
+    for letter, count in d.items():
+        print(letter, "*" * count)
+
+
+if __name__ == "__main__":
+    histogram("statistically")
