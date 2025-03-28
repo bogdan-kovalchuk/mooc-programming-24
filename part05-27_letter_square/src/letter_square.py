9b05b9350b282664cc263a6431539bf2fe2d63bb
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-28 16:05:50 +0200

    part05-27 solution

--- part05-27_letter_square/src/letter_square.py
+++ part05-27_letter_square/src/letter_square.py
@@ -1,1 +1,20 @@
-# Write your solution here
+import string
+
+layers = int(input("Layers:"))
+letters = list(string.ascii_uppercase)
+size = 2 * layers - 1
+box = [[0] * size for _ in range(size)]
+
+k = size // 2
+for i in range(k + 1):
+    for c in range(-i, i + 1):
+        box[k + i][k + c] = letters[i]
+        box[k - i][k + c] = letters[i]
+        box[k + c][k + i] = letters[i]
+        box[k + c][k - i] = letters[i]
+
+
+for row in box:
+    for val in row:
+        print(val, end="")
+    print()
