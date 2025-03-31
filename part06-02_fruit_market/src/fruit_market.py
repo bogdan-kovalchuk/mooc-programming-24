bcd1c51df66a7c88843b342c232cdede782d9aec
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-31 15:08:05 +0300

    part06-02 solution

--- part06-02_fruit_market/src/fruit_market.py
+++ part06-02_fruit_market/src/fruit_market.py
@@ -1,1 +1,11 @@
-# write your solution here
+def read_fruits() -> dict:
+    fruits = {}
+    with open("fruits.csv") as new_file:
+        for line in new_file:
+            fruit, weight = line.rstrip().split(";")
+            fruits[fruit] = float(weight)
+    return fruits
+
+
+if __name__ == "__main__":
+    print(read_fruits())
