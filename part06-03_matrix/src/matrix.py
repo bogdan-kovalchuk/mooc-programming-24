4bc2d571fec47cd4dbf3ae408fcecbbd7dfb6565
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-03-31 15:40:39 +0300

    part06-03 solution

--- part06-03_matrix/src/matrix.py
+++ part06-03_matrix/src/matrix.py
@@ -1,1 +1,23 @@
-# write your solution here
+def load_matrix():
+    file_name = "matrix.txt"
+    with open(file_name) as f:
+        return [list(map(int, line.rstrip().split(","))) for line in f]
+
+
+def matrix_sum():
+    return sum([sum(row) for row in load_matrix()])
+
+
+def matrix_max():
+    return max([max(row) for row in load_matrix()])
+
+
+def row_sums():
+    return [sum(row) for row in load_matrix()]
+
+
+if __name__ == "__main__":
+    print(load_matrix())
+    print(matrix_sum())
+    print(matrix_max())
+    print(row_sums())
