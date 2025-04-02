9a44db5ddf83e2bcefee9f138758b3169ef6d7e8
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-02 15:36:40 +0300

    part06-06 solution

--- part06-07_spellchecker/src/spellchecker.py
+++ part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,10 @@
-# write your solution here
+with open("wordlist.txt") as f:
+    wordlist = [word.rstrip() for word in f]
+
+text = input("Write text: ")
+out_text = []
+for word in text.split():
+    if word.lower() not in wordlist:
+        word = f"*{word}*"
+    out_text.append(word)
+print(" ".join(out_text))
