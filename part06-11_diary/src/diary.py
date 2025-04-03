ab6212854768a76fa5bdbfe5a5d7b8eb2f38a61b
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-03 15:54:49 +0300

    part06-11 solution

--- part06-11_diary/src/diary.py
+++ part06-11_diary/src/diary.py
@@ -1,1 +1,17 @@
-# Write your solution here
+file_name = "diary.txt"
+while True:
+    print("1 - add an entry, 2 - read entries, 0 - quit")
+    function = input("Function:")
+    if function == "1":
+        entry = input("Diary entry:")
+        with open(file_name, "a") as f:
+            f.write(entry + "\n")
+        print("Diary saved")
+    elif function == "2":
+        print("Entries:")
+        with open(file_name) as f:
+            for line in f:
+                print(line.rstrip())
+    elif function == "0":
+        print("Bye now!")
+        break
