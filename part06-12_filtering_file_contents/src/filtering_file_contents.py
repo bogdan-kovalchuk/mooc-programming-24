91b551929686e2b6eaeda5357762419abdc312d6
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-04 09:50:07 +0300

    part06-12 solution

--- part06-12_filtering_file_contents/src/filtering_file_contents.py
+++ part06-12_filtering_file_contents/src/filtering_file_contents.py
@@ -1,1 +1,23 @@
-# Write your solution here
+def filter_solutions():
+    with (
+        open("solutions.csv") as f_solutions,
+        open("correct.csv", "w") as f_correct,
+        open("incorrect.csv", "w") as f_incorrect,
+    ):
+        for line in f_solutions:
+            _, expression, result = line.split(";")
+            if "+" in expression:
+                num1, num2 = map(int, expression.split("+"))
+                num = num1 + num2
+            elif "-" in expression:
+                num1, num2 = map(int, expression.split("-"))
+                num = num1 - num2
+            elif "/" in expression:
+                num1, num2 = map(int, expression.split("/"))
+                num = num1 / num2
+            elif "*" in expression:
+                num1, num2 = map(int, expression.split("*"))
+                num = num1 * num2
+
+            target_file = f_correct if num == int(result) else f_incorrect
+            target_file.write(line)
