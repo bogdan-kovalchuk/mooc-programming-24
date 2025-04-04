1496aa3225447d117f464f1b2bef5eeb33df13ef
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-04 15:29:52 +0300

    part06-15 solution

--- part06-15_word_search/src/word_search.py
+++ part06-15_word_search/src/word_search.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def find_words(search_term: str):
+    with open("words.txt") as f:
+        words = [line.strip() for line in f]
+
+    def matches(word):
+        if search_term == word:
+            return True
+        if search_term.startswith("*") and word.endswith(search_term[1:]):
+            return True
+        if search_term.endswith("*") and word.startswith(search_term[:-1]):
+            return True
+        if "." in search_term and len(search_term) == len(word):
+            return all(sc == wc or sc == "." for sc, wc in zip(search_term, word))
+        return False
+
+    return [word for word in words if matches(word)]
