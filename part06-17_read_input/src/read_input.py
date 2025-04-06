2f89139b68412e2f21810c17cd35ef874ff23e7f
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-06 19:24:17 +0300

    part06-17 solution

--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -1,1 +1,12 @@
-# Write your solution here
+def read_input(phrase, lower_input, upper_input):
+    while True:
+        try:
+            number = int(input(phrase))
+            if number < lower_input or number > upper_input:
+                raise ValueError
+            print(f"You typed in: {number}")
+            return number
+        except ValueError:
+            print(
+                f"You must type in an integer between {lower_input} and {upper_input}"
+            )
