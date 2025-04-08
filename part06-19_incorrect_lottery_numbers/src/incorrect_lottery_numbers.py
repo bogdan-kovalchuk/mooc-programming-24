8df7495dcb53922a5776170049b2d4fe97cbe530
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-08 14:49:11 +0300

    part06-19 solution

--- part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
+++ part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def filter_incorrect():
+    with open("lottery_numbers.csv") as f_in, open("correct_numbers.csv", "w") as f_out:
+        for line in f_in:
+            try:
+                week_part, numbers_part = line.split(";")
+                nums = list(map(float, numbers_part.split(",")))
+                if not (
+                    any(not num.is_integer() for num in nums)
+                    or int(week_part.split()[1]) > 51
+                    or len(nums) != 7
+                    or max(nums) > 39
+                    or min(nums) < 1
+                ):
+                    f_out.write(line)
+            except Exception as e:
+                continue
