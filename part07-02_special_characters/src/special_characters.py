823b39acff04874dfadcce5a25133bce27955c62
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-09 16:47:55 +0300

    part07-02 solution

--- part07-02_special_characters/src/special_characters.py
+++ part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,17 @@
-# Write your solution here
+import string
+
+
+def separate_characters(my_string: str):
+    letters = ""
+    punctuation = ""
+    others = ""
+
+    for char in my_string:
+        if char in string.ascii_letters:
+            letters += char
+        elif char in string.punctuation:
+            punctuation += char
+        else:
+            others += char
+
+    return (letters, punctuation, others)
