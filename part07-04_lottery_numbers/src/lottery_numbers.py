f43c919440b102b10e83dbd9a6044628b5cb6d37
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-10 10:47:55 +0300

    part07-04 solution

--- part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,6 @@
-# Write your solution here
+from random import sample
+
+
+def lottery_numbers(amount: int, lower: int, upper: int):
+    number_pool = list(range(lower, upper + 1))
+    return sorted(sample(number_pool, amount))
