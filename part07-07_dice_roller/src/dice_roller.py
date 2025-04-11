acb396872f20cda385a430f7bbf467867fd49480
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-11 16:08:51 +0300

    part07-07 solution

--- part07-07_dice_roller/src/dice_roller.py
+++ part07-07_dice_roller/src/dice_roller.py
@@ -1,1 +1,19 @@
-# Write your solution here
+import random
+
+
+def roll(die: str):
+    return random.choice({"A": [3, 3, 3, 3, 3, 6], "B": [2, 2, 2, 5, 5, 5], "C": [1, 4, 4, 4, 4, 4]}[die])
+
+
+def play(die1: str, die2: str, times: int):
+    out = [0, 0, 0]
+    for i in range(times):
+        one = roll(die1)
+        two = roll(die2)
+        if one > two:
+            out[0] += 1
+        elif one < two:
+            out[1] += 1
+        else:
+            out[2] += 1
+    return tuple(out)
