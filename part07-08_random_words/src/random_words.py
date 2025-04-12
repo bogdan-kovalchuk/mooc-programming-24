45f70c09be718e0a7d4a27b4a2f0885fbd55d9ad
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-12 21:51:07 +0300

    part07-08 solution

--- part07-08_random_words/src/random_words.py
+++ part07-08_random_words/src/random_words.py
@@ -1,1 +1,6 @@
-# Write your solution here
+import random
+
+
+def words(n: int, beginning: str):
+    with open("words.txt") as f:
+        return random.sample([w.rstrip() for w in f if w.startswith(beginning)], n)
