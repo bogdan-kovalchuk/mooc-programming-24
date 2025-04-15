43d6c427c970873adfdebea7c7b7d4099d367deb
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-15 12:12:44 +0300

    part07-03 solution P1

--- part07-13_course_statistics/src/course_statistics.py
+++ part07-13_course_statistics/src/course_statistics.py
@@ -1,1 +1,13 @@
-# Write your solution here
+import urllib.request
+import json
+
+
+def retrieve_all():
+    url = "https://studies.cs.helsinki.fi/stats-mock/api/courses"
+    with urllib.request.urlopen(url) as response:
+        courses = json.load(response)
+
+    return [(course["fullName"], course["name"], course["year"], sum(course["exercises"])) for course in courses]
+
+
+print(retrieve_all())
