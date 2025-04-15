df56fc18566d9dfa262a90757f2363bc82d87128
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-15 14:27:43 +0300

    part07-03 solution

--- part07-13_course_statistics/src/course_statistics.py
+++ part07-13_course_statistics/src/course_statistics.py
@@ -1,13 +1,33 @@
 import urllib.request
 import json
+import math
 
 
 def retrieve_all():
     url = "https://studies.cs.helsinki.fi/stats-mock/api/courses"
-    with urllib.request.urlopen(url) as response:
-        courses = json.load(response)
+    response = urllib.request.urlopen(url)
+    courses = json.load(response)
+    return [
+        (course["fullName"], course["name"], course["year"], sum(course["exercises"]))
+        for course in courses
+        if course["enabled"]
+    ]
 
-    return [(course["fullName"], course["name"], course["year"], sum(course["exercises"])) for course in courses]
 
+def retrieve_course(course_name: str):
+    course_url = f"https://studies.cs.helsinki.fi/stats-mock/api/courses/{course_name}/stats"
+    response = urllib.request.urlopen(course_url)
+    data = json.load(response)
 
-print(retrieve_all())
+    return {
+        "weeks": len(data),
+        "students": max(c["students"] for _, c in data.items()),
+        "hours": sum(c["hour_total"] for _, c in data.items()),
+        "hours_average": math.floor(
+            sum(c["hour_total"] for _, c in data.items()) / max(c["students"] for _, c in data.items())
+        ),
+        "exercises": sum(c["exercise_total"] for _, c in data.items()),
+        "exercises_average": math.floor(
+            sum(c["exercise_total"] for _, c in data.items()) / max(c["students"] for _, c in data.items())
+        ),
+    }
