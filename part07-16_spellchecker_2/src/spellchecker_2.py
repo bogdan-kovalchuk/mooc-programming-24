d9a3def1148e41f4fd9f2cb62f1e10bb5ae3415f
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-16 14:28:56 +0300

    part07-16 solution

--- part07-16_spellchecker_2/src/spellchecker_2.py
+++ part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,20 @@
-# Write your solution here
+from difflib import get_close_matches
+
+with open("wordlist.txt") as f:
+    wordlist = [word.rstrip() for word in f]
+
+text = input("Write text: ")
+
+out_text = []
+suggestions = {}
+for word in text.split():
+    if word.lower() not in wordlist:
+        suggestions[word] = get_close_matches(word, wordlist)
+        word = f"*{word}*"
+    out_text.append(word)
+
+print(" ".join(out_text))
+if suggestions:
+    print("suggestions:")
+    for word, suggestion in suggestions.items():
+        print(f"{word}: {", ".join(suggestion)}")
