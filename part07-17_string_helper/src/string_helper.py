591ee59a1d7b044a33426bba0e0d9ab61952692d
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-16 15:25:43 +0300

    part07-17 solution

--- part07-17_string_helper/src/string_helper.py
+++ part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,24 @@
-# Write your solution here
+import string
+
+
+def change_case(orig_string: str):
+    letters = list(orig_string)
+    for i, letter in enumerate(letters):
+        if letter.isupper():
+            letters[i] = letter.lower()
+        elif letter.islower():
+            letters[i] = letter.upper()
+    return "".join(letters)
+
+
+def split_in_half(orig_string: str):
+    i = int(len(orig_string) / 2)
+    return orig_string[:i], orig_string[i:]
+
+
+def remove_special_characters(orig_string: str):
+    new_string = list()
+    for char in orig_string:
+        if char.isalnum() or char.isspace():
+            new_string.append(char)
+    return "".join(new_string)
