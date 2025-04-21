d378d1763112a9bb8b2088c1e898281bc3b48592
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-21 12:22:54 +0300

    part08-11 solution

--- part08-11_first_and_last_name/src/first_and_last_name.py
+++ part08-11_first_and_last_name/src/first_and_last_name.py
@@ -1,14 +1,21 @@
-# Write your solution here:
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+
+    def return_first_name(self):
+        first_name, _ = self.name.split()
+        return first_name
+
+    def return_last_name(self):
+        _, last_name = self.name.split()
+        return last_name
 
 
+if __name__ == "__main__":
+    peter = Person("Peter Pythons")
+    print(peter.return_first_name())
+    print(peter.return_last_name())
 
-
-
-
-
-
-
-
-
-
-
+    paula = Person("Paula Pythonnen")
+    print(paula.return_first_name())
+    print(paula.return_last_name())
