c82e79447f838eb9541cf3307a91170f8e8b06ea
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-22 10:54:30 +0300

    part08-14 solution

--- part08-14_clock/src/clock.py
+++ part08-14_clock/src/clock.py
@@ -1,1 +1,48 @@
-# Write your solution here:
+class Clock:
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def tick(self):
+        if self.seconds < 59:
+            self.seconds += 1
+        elif self.minutes < 59:
+            self.minutes += 1
+            self.seconds = 0
+        elif self.hours < 23:
+            self.hours += 1
+            self.minutes = 0
+            self.seconds = 0
+        else:
+            self.hours = 0
+            self.minutes = 0
+            self.seconds = 0
+
+    def set(self, hours: int, minutes: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = 0
+
+    def __str__(self):
+        return f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}"
+
+
+if __name__ == "__main__":
+    clock = Clock(23, 59, 55)
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+
+    clock.set(12, 5)
+    print(clock)
