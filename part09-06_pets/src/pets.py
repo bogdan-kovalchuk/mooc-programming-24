3846c699f896fc5168254a05c71a0c8bd0b685e8
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-27 19:20:00 +0300

    part09-06 solution

--- part09-06_pets/src/pets.py
+++ part09-06_pets/src/pets.py
@@ -1,5 +1,3 @@
-# WRITE YOUR SOLUTION HERE:
-
 class Pet:
     def __init__(self, name: str, description: str):
         self.name = name
@@ -8,10 +6,18 @@ class Pet:
     def __str__(self):
         return f"{self.name} ({self.description})"
 
+
 class Person:
     def __init__(self, name: str, pet: Pet):
         self.name = name
         self.pet = pet
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, whose pal is {self.pet.name}, a {self.pet.description}"
+
+
+if __name__ == "__main__":
+    hulda = Pet("Hulda", "mixed-breed dog")
+    levi = Person("Levi", hulda)
+
+    print(levi)
