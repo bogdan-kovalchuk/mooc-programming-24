17313bb22ea1738546d4ac3b393c43c940a18197
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-28 11:00:54 +0300

    part09-10 solution

--- part09-10_recording/src/recording.py
+++ part09-10_recording/src/recording.py
@@ -1,1 +1,15 @@
-# WRITE YOUR SOLUTION HERE:
+class Recording:
+    def __init__(self, length):
+        if length < 0:
+            raise ValueError("The lenght must not be below zero")
+        self.__length = length
+
+    @property
+    def length(self):
+        return self.__length
+
+    @length.setter
+    def length(self, length):
+        if length < 0:
+            raise ValueError("The lenght must not be below zero")
+        self.__length = length 
