da498fa6378a9abdd953a564cf9091052442d304
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-04-30 10:27:04 +0300

    Part09-11 solution

--- part09-11_weather_station/src/weather_station.py
+++ part09-11_weather_station/src/weather_station.py
@@ -1,3 +1,31 @@
-# WRITE YOUR SOLUTION HERE:
+class WeatherStation:
+    def __init__(self, station_name: str):
+        self.__station_name = station_name
+        self.__observations = []
+
+    def add_observation(self, observation: str):
+        self.__observations.append(observation)
+
+    def latest_observation(self):
+        if self.number_of_observations():
+            return self.__observations[-1]
+        return ""
+
+    def number_of_observations(self):
+        return len(self.__observations)
+
+    def __str__(self):
+        return f"{self.__station_name}, {self.number_of_observations()} observations"
 
 
+if __name__ == "__main__":
+    station = WeatherStation("Houston")
+    station.add_observation("Rain 10mm")
+    station.add_observation("Sunny")
+    print(station.latest_observation())
+
+    station.add_observation("Thunderstorm")
+    print(station.latest_observation())
+
+    print(station.number_of_observations())
+    print(station)
