922481565d613f1575449d9d386e91c13771a1ee
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-08 10:51:30 +0300

    Part10-01 solution

--- part10-01_laptop_computer/src/laptop_computer.py
+++ part10-01_laptop_computer/src/laptop_computer.py
@@ -1,4 +1,3 @@
-# Write your solution here:
 class Computer:
     def __init__(self, model: str, speed: int):
         self.__model = model
@@ -11,3 +10,17 @@ class Computer:
     @property
     def speed(self):
         return self.__speed
+
+
+class LaptopComputer(Computer):
+    def __init__(self, model: str, speed: int, weight: int):
+        super().__init__(model, speed)
+        self.__weight = weight
+
+    def __str__(self):
+        return f"{self.model}, {self.speed} MHz, {self.__weight} kg"
+
+
+if __name__ == "__main__":
+    laptop = LaptopComputer("NoteBook Pro15", 1500, 2)
+    print(laptop)
