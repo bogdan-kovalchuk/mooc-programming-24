26eee327fbfd94e21e3c4923214c131345fb1e94
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-14 16:45:28 +0300

    Part10-07 solution

--- part10-07_money/src/money.py
+++ part10-07_money/src/money.py
@@ -1,8 +1,36 @@
-# TEE RATKAISUSI TÄHÄN:
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
+        self.__euros = euros
+        self.__cents = cents
+
+    def __float__(self):
+        return self.__euros + self.__cents / 100
+
+    def __eq__(self, another):
+        return float(self) == float(another)
+
+    def __lt__(self, another):
+        return float(self) < float(another)
+
+    def __gt__(self, another):
+        return float(self) > float(another)
+
+    def __ne__(self, another):
+        return float(self) != float(another)
+
+    def __add__(self, another):
+        amount = float(self) + float(another)
+        euros = int(amount)
+        cents = round((amount - euros) * 100)
+        return Money(euros, cents)
+
+    def __sub__(self, another):
+        amount = float(self) - float(another)
+        if amount < 0:
+            raise ValueError(f"a negative result is not allowed")
+        euros = int(amount)
+        cents = round((amount - euros) * 100)
+        return Money(euros, cents)
 
     def __str__(self):
-        return f"{self.euros}.{self.cents}"
+        return f"{self.__euros}.{self.__cents:02} eur"
