d25a16cea75aee45ad6e98473ce7427171ae9bb0
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-28 12:23:33 +0300

    Part11-01 solution

--- part11-01_square_roots/src/square_roots.py
+++ part11-01_square_roots/src/square_roots.py
@@ -1,1 +1,11 @@
-# WRITE YOUR SOLUTION HERE:
+from math import sqrt
+
+
+def square_roots(lst: list):
+    return [sqrt(item) for item in lst]
+
+
+if __name__ == "__main__":
+    lines = square_roots([1, 2, 3, 4])
+    for line in lines:
+        print(line)
