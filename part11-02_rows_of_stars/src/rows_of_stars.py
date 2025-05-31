8dc4433c9046163ebce018c15a1695e678cad395
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-05-31 20:11:20 +0300

    Part11-02 solution

--- part11-02_rows_of_stars/src/rows_of_stars.py
+++ part11-02_rows_of_stars/src/rows_of_stars.py
@@ -1,1 +1,14 @@
-# WRITE YOUR SOLUTION HERE:
+def rows_of_stars(lst: list):
+    return ["*" * stars for stars in lst]
+
+
+if __name__ == "__main__":
+    rows = rows_of_stars([1, 2, 3, 4])
+    for row in rows:
+        print(row)
+
+    print()
+
+    rows = rows_of_stars([4, 3, 2, 1, 10])
+    for row in rows:
+        print(row)
