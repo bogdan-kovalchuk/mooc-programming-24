e864444d1a2c4f7a83e5e8838cdaa97ebf2a9e39
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-19 16:35:03 +0300

    Part11-07 solution P2

--- part11-07_lottery_numbers/src/lottery_numbers.py
+++ part11-07_lottery_numbers/src/lottery_numbers.py
@@ -6,8 +6,15 @@ class LotteryNumbers:
     def number_of_hits(self, numbers: list):
         return len([n for n in numbers if n in self.nums])
 
+    def hits_in_place(self, numbers):
+        return [n if n in self.nums else -1 for n in numbers]
+
 
 if __name__ == "__main__":
     week5 = LotteryNumbers(5, [1, 2, 3, 4, 5, 6, 7])
     my_numbers = [1, 4, 7, 11, 13, 19, 24]
     print(week5.number_of_hits(my_numbers))
+
+    week8 = LotteryNumbers(8, [1, 2, 3, 10, 20, 30, 33])
+    my_numbers = [1, 4, 7, 10, 11, 20, 30]
+    print(week8.hits_in_place(my_numbers))
