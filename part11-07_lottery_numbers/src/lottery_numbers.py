ef442d522147b52a2392c1359226a55ffee9e2ec
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-19 16:29:41 +0300

    Part11-07 solution P1

--- part11-07_lottery_numbers/src/lottery_numbers.py
+++ part11-07_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,13 @@
-# WRITE YOUR SOLUTION HERE:
+class LotteryNumbers:
+    def __init__(self, week_number: int, nums: list):
+        self.week_number = week_number
+        self.nums = nums
+
+    def number_of_hits(self, numbers: list):
+        return len([n for n in numbers if n in self.nums])
+
+
+if __name__ == "__main__":
+    week5 = LotteryNumbers(5, [1, 2, 3, 4, 5, 6, 7])
+    my_numbers = [1, 4, 7, 11, 13, 19, 24]
+    print(week5.number_of_hits(my_numbers))
