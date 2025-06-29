1b331ec4b27ffe3da6134b15b22a705c0dfa8df2
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-06-29 13:36:05 +0300

    Part11-12 solution

--- part11-12_most_common_words/src/most_common_words.py
+++ part11-12_most_common_words/src/most_common_words.py
@@ -1,1 +1,16 @@
-# WRITE YOUR SOLUTION HERE:
+import string
+
+
+def most_common_words(filename: str, lower_limit: int) -> dict:
+    with open(filename, "r", encoding="utf-8") as file:
+        text = file.read()
+
+    text_no_punct = text.translate(str.maketrans("", "", string.punctuation))
+
+    words = text_no_punct.split()
+
+    word_counts = {}
+    for word in words:
+        word_counts[word] = word_counts.get(word, 0) + 1
+
+    return {word: count for word, count in word_counts.items() if count >= lower_limit}
