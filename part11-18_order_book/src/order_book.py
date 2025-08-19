8ae6b36e0d69925702fcd2114ba2fb239fd1f93e
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2025-08-19 15:27:22 +0300

    Part11-18 solution part2

--- part11-18_order_book/src/order_book.py
+++ part11-18_order_book/src/order_book.py
@@ -20,7 +20,24 @@ class Task:
         return f"{self.id}: {self.description} ({self.workload} hours), programmer {self.programmer} {finished[self._is_finished]}"
 
 
+class OrderBook:
+    def __init__(self):
+        self.orders = []
+        self.all_programmers = set()
+
+    def add_order(self, description, programmer, workload):
+        self.all_programmers.add(programmer)
+        self.orders.append(Task(description, programmer, workload))
+
+    def all_orders(self):
+        return self.orders
+
+    def programmers(self):
+        return list(self.all_programmers)
+
+
 if __name__ == "__main__":
+    print("##### Part 1 #####")
     t1 = Task("program hello world", "Eric", 3)
     print(t1.id, t1.description, t1.programmer, t1.workload)
     print(t1)
@@ -32,3 +49,17 @@ if __name__ == "__main__":
     t3 = Task("program mobile app for workload accounting", "Eric", 25)
     print(t2)
     print(t3)
+
+    print("\n##### Part 2 #####")
+    orders = OrderBook()
+    orders.add_order("program webstore", "Adele", 10)
+    orders.add_order("program mobile app for workload accounting", "Eric", 25)
+    orders.add_order("program app for practising mathematics", "Adele", 100)
+
+    for order in orders.all_orders():
+        print(order)
+
+    print()
+
+    for programmer in orders.programmers():
+        print(programmer)
